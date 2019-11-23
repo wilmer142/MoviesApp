39e8c8e70ff2d4b5979c8c327c9843fd31ef4f2a
wilmer142 <wilmercollege@gmail.com>
2019-11-23 10:01:53 -0500

    Creating components, routing and navbar navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'buscar', component: BuscarComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
