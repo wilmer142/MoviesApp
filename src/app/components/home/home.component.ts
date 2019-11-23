4d66d1f2414c24debad1ef078927466b5e2391fe
wilmer142 <wilmercollege@gmail.com>
2019-11-23 10:25:53 -0500

    Design home html

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PeliculasService } from 'src/app/services/peliculas.service';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public peliculasService:PeliculasService) { 
+    this.peliculasService.getCartelera()
+      .subscribe( data => {
+        
+      })
+  }
 
   ngOnInit() {
   }
---
+++ src/app/services/peliculas.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+import { Jsonp } from "@angular/http";
+import 'rxjs/Rx'; // Map
+
+@Injectable()
+export class PeliculasService {
+
+  private apikey:string = "065850c708338761ee2c46c799a5aac0";
+  private urlMoviedb:string = "https://api.themoviedb.org/3";
+
+  constructor( private jsonp:Jsonp ) { }
+
+  getCartelera(){
+    let desde = new Date();
+    let hasta = new Date();
+    hasta.setDate(hasta.getDate() + 7);
+
+    let desdeStr = `${desde.getFullYear()}-${desde.getMonth()+1}-${desde.getDay()}`;
+    let hastaStr = `${desde.getFullYear()}-${desde.getMonth()+1}-${desde.getDay()}`;
+
+    let url = `${ this.urlMoviedb }/discover/movie?primary_release_date.gte=${desdeStr}&primary_release_date.lte=${hastaStr}&api_key=${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
+
+    return this.jsonp.get( url )
+                .map( res=> res.json());
+  }
+
+  getPopulares(){
+
+    let url = `${ this.urlMoviedb }/discover/movie?sort_by=popularity.desc&api_key=${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
+
+    return this.jsonp.get( url )
+                .map( res=> res.json());
+  }
+
+  buscarPelicula( texto:string ){
+
+    let url = `${ this.urlMoviedb }/search/movie?query=${ texto }&sort_by=popularity.desc&api_key=${ this.apikey }&language=es&callback=JSONP_CALLBACK`;
+
+    return this.jsonp.get( url )
+                .map( res=> res.json());
+  }
+
+}
